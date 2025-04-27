0b2acd68eedd3e17e6e54fdd88def51f774b357c
Tenzin <tenzingaycha@dhcp-10-8-030-010.mobile.reshsg.uci.edu>
2025-04-27 14:13:44 -0700

    completed project file format

---
+++ include/bank.hpp
@@ -0,0 +1,20 @@
+#ifndef BANK_CPP
+#define BANK_CPP
+
+/*
+ * Welcome to C++ Project 'BANK' 
+ *
+ */
+class Bank
+{
+private: 
+    int id;                         
+    double checking_bal = 0;        
+    double saving_bal = 0;          
+
+public: 
+    
+};  
+
+
+#endif
